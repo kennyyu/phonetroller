79faa0eeda8ae993e40fe70aee597b9efd6905a0
Kenny Yu <yu.kenny124@gmail.com>
2013-12-22 02:44:13 -0500

    cleanup

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+// server.js
+// Sets up the http and web socket servers
+
 var WebSocketServer = require('ws').Server;
 var express = require('express');
 var http = require('http');
 var port = process.env.PORT || 5000;
-var app = express();
 
 // Create an express application
 var app = express();
@@ -17,7 +19,7 @@ var server = http.createServer(app);
 server.listen(port);
 console.log("http server listening on port %d", port);
 
-// Start the websocket server
+// Start the websocket server on the same port
 var wss = new WebSocketServer({server: server});
 wss.on('connection', function(ws) {
   ws.on('message', function(message) {
